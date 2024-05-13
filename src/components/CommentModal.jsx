de6c4bb6d9c94b14696d9e53468697789265da62
Rtanakar <ratnakarmishra@623gmail.com>
2024-05-13 10:43:25 +0530

    Add comment functionality to the commentModal component and update icons component to show the number of comments

--- src/components/CommentModal.jsx
+++ src/components/CommentModal.jsx
@@ -5,8 +5,16 @@ import { useRecoilState } from "recoil";
 import Modal from "react-modal";
 import { HiX } from "react-icons/hi";
 import { useEffect, useState } from "react";
-import { doc, getFirestore, onSnapshot } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  getFirestore,
+  onSnapshot,
+  serverTimestamp,
+} from "firebase/firestore";
 import { app } from "@/firebase";
+import { useRouter } from "next/navigation";
 const { useSession } = require("next-auth/react");
 
 export default function CommentModal() {
@@ -16,6 +24,8 @@ export default function CommentModal() {
   const [post, setPost] = useState({});
   const { data: session } = useSession();
   const db = getFirestore(app);
+  const router = useRouter();
+
   useEffect(() => {
     if (postId !== "") {
       const postRef = doc(db, "posts", postId);
@@ -30,7 +40,23 @@ export default function CommentModal() {
     }
   }, [postId]);
 
-  const sendComment = async () => {};
+  const sendComment = async () => {
+    addDoc(collection(db, "posts", postId, "comments"), {
+      name: session.user.name,
+      username: session.user.username,
+      userImg: session.user.image,
+      comment: input,
+      timestamp: serverTimestamp(),
+    })
+      .then(() => {
+        setInput("");
+        setOpen(false);
+        router.push(`/posts/${postId}`);
+      })
+      .catch((error) => {
+        console.error("Error adding document: ", error);
+      });
+  };
 
   return (
     <div>
